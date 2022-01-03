31cdde6ab95498d323153bc3fc4e43d5f930e6a1
wleung85 <wleungkh@gmail.com>
2022-01-03 18:21:05 -0500

    Hooked up functionality to talk to Solana devnet and talk to deployed Metaplex Candy Machine

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import CandyMachine from './CandyMachine';
 
 // Constants
 const TWITTER_HANDLE = 'wl_devs';
@@ -70,10 +71,11 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
-          <p className="sub-text">NFT drop machine with fair mint</p>
+          <p className="header">✨ Star Wars NFT Drop ✨</p>
+          <p className="sub-text">NFT drop machine with fair mint (using Metaplex)</p>
           {!walletAddress && renderNotConnectedContainer()}
         </div>
+        {walletAddress && <CandyMachine walletAddress={window.solana}/>}
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
